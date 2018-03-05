fdf4d9b3782ab67b772964731fcd6cf473e6f673
VernL <vernon.lillies@gmail.com>
2018-03-05 18:08:15 -0500

    feat: remove unused packages and uglify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
-let uglify = require('gulp-uglify');
-let rename = require('gulp-rename');
 let imagemin = require('gulp-imagemin');
 
 gulp.task('minify-css', () => {
@@ -10,17 +8,10 @@ gulp.task('minify-css', () => {
         .pipe(gulp.dest('stylesheets'));
 });
 
-gulp.task('uglify', function() {
-    return gulp.src('assets/javascripts/**/*.js')
-        .pipe(uglify())
-        .pipe(rename({extname: ".min.js"}))
-        .pipe(gulp.dest('javascripts'));
-});
-
 gulp.task('imagin', () =>
 gulp.src('assets/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('stylesheets'))
 );
 
-gulp.task('default', ['minify-css', 'uglify', 'imagin']);
+gulp.task('default', ['minify-css', 'imagin']);
